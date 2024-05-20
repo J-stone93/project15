f2df5acca85193992c2b293da615efd42dcd75a1
J-stone93 <treous11@gmail.com>
2024-05-20 10:02:33 +0900

    init

--- src/main/Quiz9.java
+++ src/main/Quiz9.java
@@ -7,20 +7,24 @@ import java.io.OutputStreamWriter;
 
 public class Quiz9 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// 다운로드 폴더에 있는 파일의 목록을 읽어서 텍스트 파일에 출력하세요
 		// 만약 하위 폴더가 있다면 하위폴더의 파일목록도 출력하세요.
 		
-		String filedir = "C:\\Users\\G201\\Downloads";
+		// 폴더 경로
+		String filepapth = "C:\\Users\\G201\\Downloads";
 		
-		ListFile(filedir);
+		// 출력 스트림
+		FileWriter fw = new FileWriter("quiz9.txt");
+		
+		printFileList(filepapth, fw, 0);
+		
 		
 //		try {
 //			FileWriter fw = new FileWriter("quiz9.txt");
-//			OutputStreamWriter osw = new OutputStreamWriter(fw);
 //			
-//			fw.write(filedir);
+//			
 //			
 //			fw.flush();
 //		} catch (IOException e) {
@@ -31,19 +35,69 @@ public class Quiz9 {
 	}
 
 	// 재귀함수 
-    private static void ListFile( String strDirPath ) { 
-         
-        File path = new File( strDirPath ); 
-        File[] fList = path.listFiles(); 
-         
-        for( int i = 0; i < fList.length; i++ ) { 
-             
-            if( fList[i].isFile() ) { 
-                System.out.println( fList[i].getPath() );  
-            } 
-            else if( fList[i].isDirectory() ) { 
-                ListFile( fList[i].getPath() );  
-            } 
-        } 
-    } 
+//    private static void ListFile( String strDirPath ) { 
+//         
+//        File path = new File( strDirPath ); 
+//        File[] fList = path.listFiles(); 
+//         
+//        for( int i = 0; i < fList.length; i++ ) { 
+//             
+//            if( fList[i].isFile() ) { 
+//                System.out.println( fList[i].getName() );  
+//            } 
+//            else if( fList[i].isDirectory() ) { 
+//                ListFile( fList[i].getName() );  
+//            } 
+//        } 
+//    }
+	
+	// 폴더에 포함된 파일 목록을 출력하는 메소드
+	// 매개변수: 폴더경로, 출력스트림
+	public static void printFileList(String filepath, FileWriter fw, int level) throws IOException {
+
+		File dir = new File(filepath);
+		
+		// 폴더 밑에 있는 파일 목록 꺼내기
+		File[] files = dir.listFiles();
+		
+		for (int i = 0; i < files.length; i++) {
+			
+			// 배열에서 파일 꺼내기
+			File file = files[i];
+			
+			// 디렉토리 여부 확인
+			boolean isDirectory = file.isDirectory();
+			
+			// 파일 이름 꺼내기
+			String filename = file.getName();
+			
+			StringBuilder builder = new StringBuilder();
+			
+			// 들여쓰기 계층구조 표시
+			for (int j = 0; j < level; j++) {
+				builder.append("\t");
+			}
+			builder.append(filename);
+			
+			// 파일 이름 출력
+			fw.write(builder.toString());
+			
+			if (isDirectory) { // 해당 파일이 폴더라면
+				fw.write("\n");
+				String subfilepath = file.getPath();
+				
+				printFileList(subfilepath, fw, level + 1); // 자기 자신을 호출하는 재귀함수
+			}
+			fw.write("\n");
+		}
+		fw.flush(); // 문자열을 호출했을 때 버퍼를 불러줘야함
+ 	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
